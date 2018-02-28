1dbd8f94ce4deb5a58bb716d98d8593d0aa9e861
Sara Tibbetts <sxt4866@rit.edu>
2018-02-28 03:14:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,29 @@
 class Song
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def to_param
+    @name.downcase.gsub(" ", "-")
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.find_by_name(name)
+    @@all.find{ |item| item.name == name }
+  end
 end 
