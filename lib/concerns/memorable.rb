a908c3b2811a4eb3ae53a5329575a5eef945a65e
Sara Tibbetts <sxt4866@rit.edu>
2018-02-28 03:23:03 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,4 +2,8 @@ module Memorable
   def reset_all
     all = []
   end
+
+  def self.count
+    @@all.length
+  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
   extend Memorable
-  
+
   attr_accessor :name, :artist
 
   @@all = []
