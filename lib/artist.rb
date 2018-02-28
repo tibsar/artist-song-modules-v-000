6f066b595be91b63fae187b62d47580223eaca61
Sara Tibbetts <sxt4866@rit.edu>
2018-02-28 03:12:14 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,11 +2,9 @@ class Artist
   attr_accessor :name
 
   @@all = []
-  @@count = 0
 
   def initialize
     @@all << self
-    @@count += 1
   end
 
   def to_param
@@ -15,7 +13,6 @@ class Artist
 
   def self.reset_all
     @@all = [] 
-    @@count = 0 
   end
 
   def self.all
@@ -23,6 +20,6 @@ class Artist
   end
 
   def self.count
-    @@count
+    @@all.length 
   end
 end
